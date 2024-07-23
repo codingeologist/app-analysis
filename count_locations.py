2342a0abbcadc7e8fbec285c86b18e7a955ed7a9
codingeologist <codingeologist@gmail.com>
2024-07-23 08:10:59 +0100

    visitor locations count

---
+++ count_locations.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("visitor_locations_old.csv")
+cols_to_keep = ["TIMESTAMP", "COUNTRY_NAME", "COUNTRY_CODE", "LATITUDE", "LONGITUDE"]
+
+df = df[cols_to_keep]
+df = df.groupby("COUNTRY_NAME").agg(
+    COUNT=("COUNTRY_NAME", "size"),
+    LATITUDE=("LATITUDE", "first"),
+    LONGITUDE=("LONGITUDE", "first")
+).reset_index()
+
+df.to_csv("visitor_location_count.csv", index=False)
